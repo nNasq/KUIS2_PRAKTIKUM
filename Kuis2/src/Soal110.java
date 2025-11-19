90428fbd09aa84c7c1a080e1bf55a099c5c64d68
HafizhArrasyiidS <hafizharrasyiids@gmail.com>
2025-11-19 08:45:21 +0700

    Soal 1

---
+++ Kuis2/src/Soal110.java
@@ -0,0 +1,54 @@
+// Hafizh Arrasyiid Syahbana
+// 254107060026
+// SIB 1A
+// SOAL KE-1
+
+import java.util.Scanner;
+
+public class Soal110 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Meminta input jumlah mahasiswa
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int n = sc.nextInt();
+
+        // Membaca n nilai dan menyimpannya dalam array
+        int[] nilai = new int[n];
+
+        System.out.println("\nMasukkan nilai mahasiswa:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai[i] = sc.nextInt();
+        }
+
+        // Menghitung rata-rata, jumlah lulus dan tidak lulus
+        int total = 0;
+        int lulus = 0;
+        int tidakLulus = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += nilai[i];
+
+            if (nilai[i] >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
+        }
+
+        double rataRata = (double) total / n;
+
+        //  Menampilkan seluruh nilai, rata-rata, jumlah lulus & tidak lulus
+        System.out.println("\n=== HASIL ===");
+        System.out.print("Nilai yang diinput: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(nilai[i] + " ");
+        }
+
+        System.out.println("\nRata-rata nilai: " + rataRata);
+        System.out.println("Jumlah mahasiswa lulus     : " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+    }
+}
+
